fa1a17fa2ce4a669729cdd146c813816b2625cbd
wsxdev <williamsxdev@gmail.com>
2025-04-02 09:41:29 -0500

    Init

---
+++ src/main/java/espoch/edu/ec/firstproject/FirstProject.java
@@ -0,0 +1,28 @@
+package espoch.edu.ec.firstproject;
+
+public class FirstProject {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        // int valor = 5;
+
+        /* while (valor >= 5) {
+            System.out.println("Hello " + valor);
+        } */
+        /* do {
+            System.out.println("Hello " + valor);
+            valor++;
+        } while(valor > 6); */
+
+        // area del rectangulo, calcular e imprimir
+
+        float base = 5.5f;
+        float altura = 6.6f;
+        float area = 0;
+
+        System.out.println(area = base * altura);
+
+        
+
+    }
+}
